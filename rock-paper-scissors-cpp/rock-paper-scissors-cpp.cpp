54bd3bae335182a9099a4414f91c1a8457d328ca
Alaa Alkatshah <alaaalkatshah@gmail.com>
2026-03-15 20:02:03 +0300

    feat: add data types - enums and structs

--- rock-paper-scissors-cpp/rock-paper-scissors-cpp.cpp
+++ rock-paper-scissors-cpp/rock-paper-scissors-cpp.cpp
@@ -1,20 +1,30 @@
-// rock-paper-scissors-cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include<iostream>
+using namespace std;
 
-#include <iostream>
+enum enGameChoice { Stone = 1, Paper = 2, Scissors = 3 };
+
+enum enWinner { Player1 = 1, Computer = 2, Draw = 3 };
+
+struct stRoundInfo
+{
+    short RoundNumber = 0;
+    enGameChoice Player1Choice = enGameChoice::Stone;
+    enGameChoice ComputerChoice = enGameChoice::Stone;
+    enWinner Winner = enWinner::Draw;
+    string WinnerName = "";
+};
+
+struct stGameResults
+{
+    short GameRounds = 0;
+    short Player1WinTimes = 0;
+    short Computer2WinTimes = 0;
+    short DrawTimes = 0;
+    enWinner GameWinner = enWinner::Draw;
+    string WinnerName = "";
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
