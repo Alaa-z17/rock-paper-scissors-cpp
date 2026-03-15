ea3406cd2e9678682f4f9c6e55bcaa27b9c60ddb
Alaa Alkatshah <alaaalkatshah@gmail.com>
2026-03-15 20:22:30 +0300

    feat: add Tabs, ShowGameOverScreen and ShowFinalGameResults functions

--- rock-paper-scissors-cpp/rock-paper-scissors-cpp.cpp
+++ rock-paper-scissors-cpp/rock-paper-scissors-cpp.cpp
@@ -148,7 +148,35 @@ stGameResults PlayGame(short HowManyRounds)
     }
     return FillGameResults(HowManyRounds, Player1WinTimes, ComputerWinTimes, DrawTimes);
 }
+string Tabs(short NumberOfTabs)
+{
+    string t = "";
+    for (int i = 1; i < NumberOfTabs; i++)
+    {
+        t = t + "\t";
+        cout << t;
+    }
+    return t;
+}
 
+void ShowGameOverScreen()
+{
+    cout << Tabs(2) << "__________________________________________________________\n\n";
+    cout << Tabs(2) << "             +++ G a m e  O v e r +++\n";
+    cout << Tabs(2) << "__________________________________________________________\n\n";
+}
+
+void ShowFinalGameResults(stGameResults GameResults)
+{
+    cout << Tabs(2) << "_____________________ [Game Results]_____________________\n\n";
+    cout << Tabs(2) << "Game Rounds        : " << GameResults.GameRounds << endl;
+    cout << Tabs(2) << "Player1 won times  : " << GameResults.Player1WinTimes << endl;
+    cout << Tabs(2) << "Computer won times : " << GameResults.Computer2WinTimes << endl;
+    cout << Tabs(2) << "Draw times         : " << GameResults.DrawTimes << endl;
+    cout << Tabs(2) << "Final Winner       : " << GameResults.WinnerName << endl;
+    cout << Tabs(2) << "___________________________________________________________\n";
+    SetWinnerScreenColor(GameResults.GameWinner);
+}
 int main()
 {
    srand((unsigned)time(NULL));
